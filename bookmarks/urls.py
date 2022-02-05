c8c4ab0500abda6b66a7012d8574ee4e1816ba5c
M254-bto <ngechamike26@gmail.com>
2022-02-05 23:19:22 +0300

    Full working Auth

---
+++ bookmarks/urls.py
@@ -0,0 +1,26 @@
+"""bookmarks URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+
+"""
+from django.contrib import admin
+from django.urls import path, include, reverse
+from django.contrib.auth import views as auth_views
+
+#class Logout(auth_views.LogoutView):
+ #    reverse('/')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('account.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+]
